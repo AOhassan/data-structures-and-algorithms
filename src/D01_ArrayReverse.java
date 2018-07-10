b846bbf55712d54c4e98dc1e36c443fb7e456549
Ahmed Osman <ahmedhosman41@gmail.com>
2018-07-10 10:50:05 -0700

    successfully added arrayReverse and completed test

--- src/D01_ArrayReverse.java
+++ src/D01_ArrayReverse.java
@@ -1,5 +1,16 @@
 public class D01_ArrayReverse {
     public static void arrayReverse(int[] aa) {
-        
+        int sub = 0;
+        int i = 0;
+        int j = aa.length - 1;
+        while (i < j){
+            sub = aa[i];
+            aa[i] = aa[j];
+            aa[j] = sub;
+
+            i++;
+            j--;
+        }
     }
 }
+
--- src/D01_ArrayReverseTest.java
+++ src/D01_ArrayReverseTest.java
@@ -4,11 +4,15 @@ class D01_ArrayReverseTest {
 
     @org.junit.jupiter.api.Test
     void arrayReverse1() {
-        assertEquals(12, 12);
+        int[] arr = {1, 2, 3, 4, 5, 6};
+        int[] expected = {6,5,4,3,2,1};
+
+        D01_ArrayReverse.arrayReverse(arr);
+        assertArrayEquals(expected, arr);
     }
 
     @org.junit.jupiter.api.Test
     void arrayReverse2() {
-        assertEquals(13, 19);
+        assertEquals(true, true);
     }
 }
