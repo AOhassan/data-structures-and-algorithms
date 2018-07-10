ceba3083aba58e1856d56f2d9553a836c5c7444e
Ahmed Osman <ahmedhosman41@gmail.com>
2018-07-09 18:17:02 -0700

    initial test infrastructure

---
+++ src/D01_ArrayReverse.java
@@ -0,0 +1,5 @@
+public class D01_ArrayReverse {
+    public static void arrayReverse(int[] aa) {
+        
+    }
+}
---
+++ src/D01_ArrayReverseTest.java
@@ -0,0 +1,14 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class D01_ArrayReverseTest {
+
+    @org.junit.jupiter.api.Test
+    void arrayReverse1() {
+        assertEquals(12, 12);
+    }
+
+    @org.junit.jupiter.api.Test
+    void arrayReverse2() {
+        assertEquals(13, 19);
+    }
+}
