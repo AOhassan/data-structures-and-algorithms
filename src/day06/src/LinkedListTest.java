c99e916758d9a9ae879e46ec27b49033b0f6ad67
Ahmed Osman <ahmedhosman41@gmail.com>
2018-07-17 14:50:30 -0700

    added Linkedlist methods

---
+++ src/day06/src/LinkedListTest.java
@@ -0,0 +1,21 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class LinkedListTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void append() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void insertBefore() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void insertAfter() {
+    }
+}
