0e1992d618ff6bce0f07d1beb1184d0a05d8e177
Ahmed Osman <ahmedhosman41@gmail.com>
2018-07-19 09:34:57 -0700

    branching

--- src/day08/src/MergedLinkedLists.java
+++ src/day08/src/MergedLinkedLists.java
@@ -1,5 +1,12 @@
+import java.util.LinkedList;
+
 public class MergedLinkedLists {
 
-    public 
+    public LinkedList mergedLinkedList(ListNode l1, ListNode l2) {
+        while(l1.next != null || l2.next != null){
+
+        }
+
+    }
 
 }
