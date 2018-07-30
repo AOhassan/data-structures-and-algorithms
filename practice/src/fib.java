573ec52aa90a6899c3999e873f166eca2eb8d200
Ahmed Osman <ahmedhosman41@gmail.com>
2018-07-30 10:01:53 -0700

    fixing files

---
+++ practice/src/fib.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class fib {
+    public static int fib(int n) {
+        
+        
+            if(A[n]!=-1) return A[n];
+            A[0]=0;
+            A[1]=1;
+            for i=2 to n
+            A[i]= addition of A[i],A[i-1];
+            return A[n]
+
+        return n;
+    }
+}
---
+++ src/day15/src/EeneyMeeneyMineyMoe.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Queue;
+
+public class EeneyMeeneyMineyMoe {
+
+    public static String youreOut(Queue<String> names, int n){
+
+        int i = 1;
+        while(names.size() != 1){
+
+            if(i < n){
+                names.add(names.poll());
+            } else {
+                names.poll();
+                i = 0;
+            }
+
+            i++;
+        }
+        return names.poll();
+    }
+}
+
+
+
+
