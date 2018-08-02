ba916f04f7b315560e8882c9cfc1b423b43c0999
Ahmed Osman <ahmedhosman41@gmail.com>
2018-08-01 17:05:20 -0700

    finished

--- src/day18/FindMax.java
+++ src/day18/FindMax.java
@@ -3,19 +3,18 @@ package day18;
 public class FindMax {
     TreeNode root;
 
-    public int findMax() {
-        return this.findMax(this.root);
-    }
-
     private int findMax(TreeNode node) {
-        if (node == null) {
-            return 0;
-        }
+        if (node == null) return 0;
+
         int left = this.findMax(node.left);
         int right = this.findMax(node.right);
         int recurse = Math.max(left, right);
 
         return Math.max(node.data, recurse);
+    }
+
+    public int findMax() {
+        return this.findMax(this.root);
     }
 }
 
